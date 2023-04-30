0c794e71b8a2d753191e69096fdbff8a1313b9cc
sRolick <sRolicksun@outlook.com>
2023-04-30 11:32:26 +0800

    优化部分代码

--- 黄金矿工/main.cpp
+++ 黄金矿工/main.cpp
@@ -109,79 +109,8 @@ Object* Find(List* pList, int index) {
 	return NULL;
 }
 
-//加载图片
-void LoadImages() {
-	loadimage(&i_back, _T(".\\Resources\\pictures\\level-background-0.jpg"));
-	loadimage(&i_brick, _T(".\\Resources\\pictures\\brick.png"),WINDOWS_WIDTH+30,0,true);
-	loadimage(&i_player[0], _T(".\\Resources\\pictures\\char1.jpg"));
-	loadimage(&i_player[1], _T(".\\Resources\\pictures\\char2.jpg"));
-	loadimage(&i_mplayer[0], _T(".\\Resources\\pictures\\char1_mask.jpg"));
-	loadimage(&i_mplayer[1], _T(".\\Resources\\pictures\\char2_mask.jpg"));
-	loadimage(&i_hook[0], _T(".\\Resources\\pictures\\hook.bmp"));
-	loadimage(&i_mhook[0], _T(".\\Resources\\pictures\\hook_mask.bmp"));
-	loadimage(&i_biggold, _T(".\\Resources\\pictures\\big_gold.png"));
-	loadimage(&i_mbiggold, _T(".\\Resources\\pictures\\big_gold_mask.png"));
-	loadimage(&i_smallgold, _T(".\\Resources\\pictures\\small_gold.bmp"));
-	loadimage(&i_msmallgold, _T(".\\Resources\\pictures\\small_gold_mask.bmp"));
-	loadimage(&i_diamond, _T(".\\Resources\\pictures\\diamond.png"));
-	loadimage(&i_mdiamond, _T(".\\Resources\\pictures\\diamond_mask.png"));
-	loadimage(&i_stone, _T(".\\Resources\\pictures\\stone.png"));
-	loadimage(&i_mstone, _T(".\\Resources\\pictures\\stone_mask.png"));
-	loadimage(&i_shushu_left[0], _T(".\\Resources\\pictures\\shushu.png"));
-	loadimage(&i_shushu_left[1], _T(".\\Resources\\pictures\\shushu_diamond.png"));
-	loadimage(&i_mshushu_left[0], _T(".\\Resources\\pictures\\shushu_mask.png"));
-	loadimage(&i_mshushu_left[1], _T(".\\Resources\\pictures\\shushu_diamond_mask.png"));
-	loadimage(&i_clear, _T(".\\Resources\\pictures\\clear.png"));
-	loadimage(&i_end, _T(".\\Resources\\pictures\\end.jpg"));
-	loadimage(&pause_background, _T(".\\Resources\\pictures\\pause_background.png"));
-	loadimage(&i_start, _T(".\\Resources\\pictures\\start.png"));
-	loadimage(&i_logo, _T(".\\Resources\\pictures\\logo.png"));
-	loadimage(&i_startbottom, _T(".\\Resources\\pictures\\startbottom2.png"));
-	i_hook[1] = i_hook[0];
-	i_mhook[1] = i_mhook[0];
-
-	for (int i = 0; i < 2; i++) {
-		i_shushu_right[i] = i_shushu_left[i];
-		FlipImage(&i_shushu_right[i], &i_shushu_left[i]);
-		i_mshushu_right[i] = i_mshushu_left[i];
-		FlipImage(&i_mshushu_right[i], &i_mshushu_left[i]);
-	}
-}
-
-//左右翻转图片
-void FlipImage(IMAGE* pDst, IMAGE* pSrc) {
-	DWORD* pdSrc = GetImageBuffer(pSrc);
-	DWORD* pdDst = GetImageBuffer(pDst);
-	int width = pSrc->getwidth();
-	int height = pSrc->getheight();
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			int r = GetRValue(pdSrc[x + y * width]);
-			int g = GetGValue(pdSrc[x + y * width]);
-			int b = GetBValue(pdSrc[x + y * width]);
-			pdDst[width - x + y * width] = RGB(r, g, b);
-		}
-	}
-}
-
-//利用蒙版图层打印透明背景图片
-void PutImageWithMask(int PosX, int PosY, IMAGE* pImg, IMAGE* pImgMask) {
-	putimage(PosX, PosY, pImgMask, NOTSRCERASE);
-	putimage(PosX, PosY, pImg, SRCINVERT);
-}
-
-//打印透明背景png图
-void TransparentImage(IMAGE* dstimg, int x, int y, IMAGE* srcimg) {
-	HDC dstDC = GetImageHDC(dstimg);
-	HDC srcDC = GetImageHDC(srcimg);
-	int w = srcimg->getwidth();
-	int h = srcimg->getheight();
-	BLENDFUNCTION bf = { AC_SRC_OVER,0,255,AC_SRC_ALPHA };
-	AlphaBlend(dstDC, x, y, w, h, srcDC, 0, 0, w, h, bf);
-}
-
 //获取键盘事件
-void GetKeyboard() {
+void KeyboardEvent() {
 	if (GetAsyncKeyState(27) & 0x8000) {	//ESC
 		getimage(&img, 0, 0, WINDOWS_WIDTH, WINDOWS_HEIGHT);
 		gameState = Pause;
@@ -229,19 +158,61 @@ void MouseEvent() {
 
 }
 
+//加载图片
+void LoadImages() {
+	loadimage(&i_back, _T(".\\Resources\\pictures\\level-background-0.jpg"));
+	loadimage(&i_brick, _T(".\\Resources\\pictures\\brick.png"),WINDOWS_WIDTH+30,0,true);
+	loadimage(&i_player[0], _T(".\\Resources\\pictures\\char1.jpg"));
+	loadimage(&i_player[1], _T(".\\Resources\\pictures\\char2.jpg"));
+	loadimage(&i_mplayer[0], _T(".\\Resources\\pictures\\char1_mask.jpg"));
+	loadimage(&i_mplayer[1], _T(".\\Resources\\pictures\\char2_mask.jpg"));
+	loadimage(&i_hook[0], _T(".\\Resources\\pictures\\hook.bmp"));
+	loadimage(&i_mhook[0], _T(".\\Resources\\pictures\\hook_mask.bmp"));
+	loadimage(&i_biggold, _T(".\\Resources\\pictures\\big_gold.png"));
+	loadimage(&i_mbiggold, _T(".\\Resources\\pictures\\big_gold_mask.png"));
+	loadimage(&i_smallgold, _T(".\\Resources\\pictures\\small_gold.bmp"));
+	loadimage(&i_msmallgold, _T(".\\Resources\\pictures\\small_gold_mask.bmp"));
+	loadimage(&i_diamond, _T(".\\Resources\\pictures\\diamond.png"));
+	loadimage(&i_mdiamond, _T(".\\Resources\\pictures\\diamond_mask.png"));
+	loadimage(&i_stone, _T(".\\Resources\\pictures\\stone.png"));
+	loadimage(&i_mstone, _T(".\\Resources\\pictures\\stone_mask.png"));
+	loadimage(&i_shushu_left[0], _T(".\\Resources\\pictures\\shushu.png"));
+	loadimage(&i_shushu_left[1], _T(".\\Resources\\pictures\\shushu_diamond.png"));
+	loadimage(&i_mshushu_left[0], _T(".\\Resources\\pictures\\shushu_mask.png"));
+	loadimage(&i_mshushu_left[1], _T(".\\Resources\\pictures\\shushu_diamond_mask.png"));
+	loadimage(&i_clear, _T(".\\Resources\\pictures\\clear.png"));
+	loadimage(&i_end, _T(".\\Resources\\pictures\\end.jpg"));
+	loadimage(&pause_background, _T(".\\Resources\\pictures\\pause_background.png"));
+	loadimage(&i_start, _T(".\\Resources\\pictures\\start.png"));
+	loadimage(&i_logo, _T(".\\Resources\\pictures\\logo.png"));
+	loadimage(&i_startbottom, _T(".\\Resources\\pictures\\startbottom2.png"));
+	i_hook[1] = i_hook[0];
+	i_mhook[1] = i_mhook[0];
+
+	for (int i = 0; i < 2; i++) {
+		i_shushu_right[i] = i_shushu_left[i];
+		FlipImage(&i_shushu_right[i], &i_shushu_left[i]);
+		i_mshushu_right[i] = i_mshushu_left[i];
+		FlipImage(&i_mshushu_right[i], &i_mshushu_left[i]);
+	}
+}
+
 //游戏对象初始化
 void Initialize() {
 	gameState = Begin;
 	m = GetMouseMsg();
 	srand(time(NULL));
 
+	//初始化player
 	player.x = WINDOWS_WIDTH / 2 - 50;
 	player.y = WINDOWS_HEIGHT / 2 - 240;
 	player.score = 0;
 	player.goal = 500;
-	player.timer = 0;
-	player.index = 0;
+	player.ani_timer = 0;
+	player.ani_index = 0;
+	player.isShow = false;
 
+	//初始化hook
 	hook.x = player.x+20;
 	hook.y = player.y+60;
 	hook.length = 0;
@@ -253,7 +224,7 @@ void Initialize() {
 	hook.dy = 0;
 	hook.carry = 0;
 
-	list.head = NULL;
+	list.head = NULL;	//初始化链表
 	for (int i = 0; i < BIG_GOLD_AMOUNT; i++) {		// 生成大金块
 		Object bigGold;
 		bigGold.size = rand() % 3 + 6;	//范围6-8
@@ -264,8 +235,7 @@ void Initialize() {
 		bigGold.image = &img_tmp[i][0];
 		bigGold.m_image = &img_tmp[i][1];
 		SetObjectPosition(&list, &bigGold);
-		bigGold.isMove = 0;
-		bigGold.takeAble = 0;
+		bigGold.type = Gold;
 		Add(&list, bigGold);
 	}
 	for (int i = 0; i < SMALL_GOLD_AMOUNT; i++) {	//生成小金块
@@ -278,8 +248,7 @@ void Initialize() {
 		smallGold.image = &img_tmp[BIG_GOLD_AMOUNT + i][0];
 		smallGold.m_image = &img_tmp[BIG_GOLD_AMOUNT + i][1];
 		SetObjectPosition(&list, &smallGold);
-		smallGold.isMove = 0;
-		smallGold.takeAble = 0;
+		smallGold.type = Gold;
 		Add(&list, smallGold);
 	}
 	for (int i = 0; i < STONE_AMOUNT; i++) {		//生成石头
@@ -291,22 +260,20 @@ void Initialize() {
 		SetObjectPosition(&list, &stone);
 		stone.size = 9;
 		stone.score = 10;
-		stone.isMove = 0;
-		stone.takeAble = 0;
+		stone.type = Gold;
 		Add(&list, stone);
 	}
-	for (int i = 0; i < DIAMOND_AMOUNT; i++) {		//生成钻石和鼠鼠
-		Object diamond;
+	for (int i = 0; i < DIAMOND_AMOUNT; i++) {	
+		Object diamond;			//生成钻石
 		diamond.image = &i_diamond;
 		diamond.m_image = &i_mdiamond;
 		SetObjectPosition(&list, &diamond);
 		diamond.size = 1;
 		diamond.score = 200;
-		diamond.isMove = 0;
-		diamond.takeAble = 1;
+		diamond.type = Diamond;
 		Add(&list, diamond);
-
-		Object mouse;
+			
+		Object mouse;			//生成老鼠
 		mouse.dir = rand() % 2;
 		mouse.y = diamond.y;
 		if (mouse.dir == 0) {
@@ -321,8 +288,7 @@ void Initialize() {
 		}
 		mouse.size = 1;
 		mouse.score = 10;
-		mouse.isMove = 1;
-		mouse.takeAble = -1;
+		mouse.type = Mouse;
 		Add(&list, mouse);
 	}
 
@@ -342,12 +308,12 @@ void Draw() {
 //绘制角色
 void DrawPlayer() {
 	if (hook.isThrow) {	//如果钩子投出，播放动画
-		player.timer++;
-		if (player.timer > 10) {
-			player.timer = 0;
-			player.index = (player.index + 1) % 2;
+		player.ani_timer++;
+		if (player.ani_timer > 10) {
+			player.ani_timer = 0;
+			player.ani_index = (player.ani_index + 1) % 2;
 		}
-		PutImageWithMask(player.x, player.y, &i_player[player.index], &i_mplayer[player.index]);
+		PutImageWithMask(player.x, player.y, &i_player[player.ani_index], &i_mplayer[player.ani_index]);
 	}
 	else {
 		PutImageWithMask(player.x, player.y, &i_player[0], &i_mplayer[0]);	//绘制主角
@@ -360,21 +326,41 @@ void DrawUI() {
 	settextcolor(WHITE);
 	setbkmode(TRANSPARENT);
 
+	//绘制分数
 	TCHAR scoreText[30];
 	_stprintf_s(scoreText, _T("金钱：$%d"), player.score);
 	outtextxy(10,10,scoreText);
 
+	//绘制目标分数
 	TCHAR goalText[30];
 	_stprintf_s(goalText, _T("目标钱数：$%d"), player.goal);
 	outtextxy(10, 50, goalText);
 
+	//绘制时间
 	TCHAR timeText[30];
 	_stprintf_s(timeText, _T("时间：%d"), Time);
 	outtextxy(800, 10, timeText);
 
+	//绘制玩家名称
 	TCHAR nameText[30];
 	_stprintf_s(nameText, _T("玩家名称：%s"), player.name);
 	outtextxy(800-(wcslen(player.name)*16), 50, nameText);
+
+	//绘制得分提示
+	TCHAR getScoreText[30];
+	static int duringTimer = 60;//持续时长
+	if (player.isShow)
+	{
+		_stprintf_s(getScoreText, _T("+%d"), player.getScore);
+		settextcolor(LIGHTCYAN);
+		outtextxy(player.x+40, player.y-20, getScoreText);
+		duringTimer--;
+		if (duringTimer == 0) {
+			player.isShow = false;
+			duringTimer = 60;
+		}
+	}
+
 }
 
 
@@ -468,7 +454,7 @@ void ThrowHook() {
 	hook.isBack = false;
 	hook.length += hook.speed;
 	mciSendString(_T("open .\\Resources\\sound\\dig.mp3 alias dig"), 0, 0, 0);
-	mciSendString(_T("play dig"), 0, 0, 0);
+	mciSendString(_T("play dig"), 0, 0, 0);	//播放音效
 }
 
 //收回钩子
@@ -480,7 +466,7 @@ void HookBack(int speed) {
 	}
 }
 
-//收回钩子与对象
+//收回钩子和抓取到的物品
 void HookBack(int speed, int index) {
 	hook.length -= speed;
 	Object* obj = Find(&list, index);	//通过索引找到要收回的对象
@@ -502,6 +488,8 @@ void HookBack(int speed, int index) {
 			hook.length = 0;
 			hook.carry = 0;
 			player.score += obj->score;	//将分数累加
+			player.getScore = obj->score;	//记录本次得分
+			player.isShow = true;
 			Delete(&list, index);	//删除物品
 			hook.isThrow = false;
 		}
@@ -512,62 +500,59 @@ void HookBack(int speed, int index) {
 void ShushuMove() {
 	Node* p;
 	for (p = list.head; p; p = p->next) {
-		if (p->object.isMove == 1) {	//通过节点p遍历找到老鼠
-			switch (p->object.dir)
-			{
-			case 0:	//左
+		if (p->object.type == Mouse) {	//通过节点p遍历找到老鼠
+
+			if (p->object.dir == 0) {	//老鼠向左走
 				p->object.x = p->object.x - 1;
-				{
-					Node* q;	//通过节点q遍历检测碰撞
-					for (q=list.head; q; q = q->next) {
-						if (CollisionDetect(p->object, q->object) && q->object.takeAble == 1) {	//如果抓到了钻石
+				Node* q;	//通过节点q遍历检测碰撞
+					for (q = list.head; q; q = q->next) {
+						if (CollisionDetect(p->object, q->object) && q->object.type == Diamond) {	//如果抓到了钻石
 							p->object.image = &i_shushu_left[1];
 							p->object.m_image = &i_mshushu_left[1];
 							p->object.score = 210;
-							Delete(&list, &q->object);
+							Delete(&list, &q->object);	//删除钻石
 							break;
 						}
-						if (p->object.score == 210) {	//如果已经抓到了钻石
-							if (CollisionDetect(p->object, q->object)&&q->object.takeAble==0) {	//碰到物体就返回
-								p->object.image = &i_shushu_right[1];
-								p->object.m_image = &i_mshushu_right[1];
-								p->object.dir = 1;
-							}
-						}
 					}
-				}
-				break;
-			case 1:	//右
+			}
+			else {	//老鼠向右走
 				p->object.x = p->object.x + 1;
-				{
-					Node* q;
+				Node* q;	//通过节点q遍历检测碰撞
 					for (q = list.head; q; q = q->next) {
-						if (CollisionDetect(p->object, q->object) && q->object.takeAble == 1) {	//如果抓到了钻石
+						if (CollisionDetect(p->object, q->object) && q->object.type == Diamond) {	//如果抓到了钻石
 							p->object.image = &i_shushu_right[1];
 							p->object.m_image = &i_mshushu_right[1];
 							p->object.score = 210;
 							Delete(&list, &q->object);
 							break;
 						}
-						if (p->object.score == 210) {	//如果已经抓到了钻石
-							if (CollisionDetect(p->object, q->object)&&q->object.takeAble==0) {	//碰到物体就返回
-								p->object.image = &i_shushu_left[1];
-								p->object.m_image = &i_mshushu_left[1];
-								p->object.dir = 0;
-							}
-						}
 					}
-				}
-				break;
 			}
+
 			if (p->object.score == 210) {	//如果已经抓到了钻石
 				if (p->object.x > WINDOWS_WIDTH || p->object.x+28 < 0) {
 					Delete(&list, &p->object);	//老鼠移动到屏幕外，删除
 					break;
 				}
+				Node *q;
+				for (q = list.head; q; q = q->next) {
+					if (CollisionDetect(p->object, q->object) && q->object.type == Gold) {	//如果碰到物体
+						if (p->object.dir == 0) {
+							p->object.image = &i_shushu_right[1];
+							p->object.m_image = &i_mshushu_right[1];
+							p->object.dir = 1;
+						}
+						else {
+							p->object.image = &i_shushu_left[1];
+							p->object.m_image = &i_mshushu_left[1];
+							p->object.dir = 0;
+						}
+					}
+				}
+
 			}
+
 		}
-		continue;
 	}
 
 }
@@ -596,9 +581,8 @@ void SetObjectPosition(List *pList,Object *obj) {
 			p = p->next;
 		}
 		//链表中的每一项都检测完毕
-		if (p == NULL) {
+		if (p == NULL)
 			break;
-		}
 	}
 	obj->x = x;
 	obj->y = y;
@@ -664,13 +648,33 @@ bool CollisionDetect(Object obj1, Object obj2) {
 	return false;
 }
 
+//游戏开始
+void GameBegin() {
+	Resize(NULL, i_start.getwidth(), i_start.getheight());
+	putimage(0, 0, &i_start);
+	TransparentImage(NULL, 20, 20, &i_logo);
+	putimage(90, 120, &i_startbottom);
+
+	//打印目标钱数
+	settextstyle(32, 0, L"黑体");
+	LOGFONT font;
+	gettextstyle(&font);
+	font.lfQuality = ANTIALIASED_QUALITY;	//设置输出效果为抗锯齿
+	settextstyle(&font);
+	settextcolor(YELLOW);
+	setbkmode(TRANSPARENT);
+	TCHAR goalText[30];
+	_stprintf_s(goalText, _T("目标钱数：$%d"), player.goal);
+	outtextxy(120, 400, goalText);
+}
+
 //游戏结束
 void GameOver() {
 	static int isopen = 1;
 	FILE* fp = NULL;
 	errno_t err;
 	if (isopen == 1) {
-		err=fopen_s(&fp, "test.txt", "a+, ccs=utf-8");
+		err=fopen_s(&fp, "charts.txt", "a+, ccs=utf-8");
 	}
 	wchar_t text[30];
 
@@ -708,26 +712,10 @@ void Start() {
 
 // Update 在每一帧被调用
 void Update() {
-	switch (gameState)
+	switch (gameState)	//判断游戏状态
 	{
 	case Begin:
-		Resize(NULL, i_start.getwidth(), i_start.getheight());
-		putimage(0, 0, &i_start);
-		TransparentImage(NULL, 20, 20, &i_logo);
-		putimage(90, 120, &i_startbottom);
-
-		//打印目标钱数
-		settextstyle(32, 0, L"黑体");
-		LOGFONT font;
-		gettextstyle(&font);
-		font.lfQuality = ANTIALIASED_QUALITY;	//设置输出效果为抗锯齿
-		settextstyle(&font);
-		settextcolor(YELLOW);
-		setbkmode(TRANSPARENT);
-		TCHAR goalText[30];
-		_stprintf_s(goalText, _T("目标钱数：$%d"), player.goal);
-		outtextxy(120, 400, goalText);
-
+		GameBegin();
 		if (MouseHit()) {
 			m = GetMouseMsg();
 			MouseEvent();
@@ -746,7 +734,7 @@ void Update() {
 		HookSway();	//钩子摆动
 		ShushuMove();	//老鼠移动
 
-		GetKeyboard();	//获取键盘事件
+		KeyboardEvent();	//获取键盘事件
 		if (MouseHit()) {
 			m = GetMouseMsg();
 			MouseEvent();	//获取鼠标事件
@@ -776,8 +764,22 @@ void Update() {
 		break;
 
 	}
-}
 
+	//关闭已播放完的音乐文件
+	WCHAR buffer[48];
+	mciSendString(_T("status dig mode"), buffer, _countof(buffer), 0);
+	if (wcscmp(buffer, L"stopped") == 0) {
+		mciSendString(_T("close dig"), 0, 0, 0);
+	}
+	mciSendString(_T("status low-value mode"), buffer, _countof(buffer), 0);
+	if (wcscmp(buffer, L"stopped") == 0) {
+		mciSendString(_T("close low-value"), 0, 0, 0);
+	}
+	mciSendString(_T("status normal-value mode"), buffer, _countof(buffer), 0);
+	if (wcscmp(buffer, L"stopped") == 0) {
+		mciSendString(_T("close normal-value"), 0, 0, 0);
+	}
+}
 
 int main() {
 	Start();
@@ -789,20 +791,6 @@ int main() {
 		Sleep(1000 / 60);
 		cleardevice();
 
-		//关闭已播放完的音乐文件
-		WCHAR buffer[48];
-		mciSendString(_T("status dig mode"), buffer, _countof(buffer), 0);
-		if (wcscmp(buffer, L"stopped") == 0) {
-			mciSendString(_T("close dig"), 0, 0, 0);
-		}
-		mciSendString(_T("status low-value mode"), buffer, _countof(buffer), 0);
-		if (wcscmp(buffer, L"stopped") == 0) {
-			mciSendString(_T("close low-value"), 0, 0, 0);
-		}
-		mciSendString(_T("status normal-value mode"), buffer, _countof(buffer), 0);
-		if (wcscmp(buffer, L"stopped") == 0) {
-			mciSendString(_T("close normal-value"), 0, 0, 0);
-		}
 	}
 	EndBatchDraw();
 }
